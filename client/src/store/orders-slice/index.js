6c0630133076617a4972396b10e3bbef4592c010
oliveszn <ekpealamicheal@gmail.com>
2025-06-19 16:16:21 +0100

    finished thunk

---
+++ client/src/store/orders-slice/index.js
@@ -0,0 +1,136 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  approvalURL: null,
+  isLoading: false,
+  orderId: null,
+  orderList: [],
+  orderDetails: null,
+};
+
+export const createNewOrder = createAsyncThunk(
+  "/order/createNewOrder",
+  async (orderData) => {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/order/create`,
+      orderData
+    );
+
+    return response.data;
+  }
+);
+
+export const getAllOrdersByUserId = createAsyncThunk(
+  "/order/getAllOrdersByUserId",
+  async (userId) => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/order/list/`,
+      { withCredentials: true }
+    );
+
+    return response.data;
+  }
+);
+
+export const getOrderDetails = createAsyncThunk(
+  "/order/getOrderDetails",
+  async (id) => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/order/details/${id}`
+    );
+
+    return response.data;
+  }
+);
+
+/////FOR ADMIN
+export const getAllOrdersForAdmin = createAsyncThunk(
+  "/order/getAllOrdersForAdmin",
+  async () => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/order/get`
+    );
+
+    return response.data;
+  }
+);
+
+export const updateOrderStatus = createAsyncThunk(
+  "/order/updateOrderStatus",
+  async ({ id, orderStatus }) => {
+    const response = await axios.put(
+      `${import.meta.env.VITE_API_URL}/api/order/status/${id}`,
+      {
+        orderStatus,
+      }
+    );
+
+    return response.data;
+  }
+);
+
+const orderSlice = createSlice({
+  name: "orderSlice",
+  initialState,
+  reducers: {
+    resetOrderDetails: (state) => {
+      state.orderDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNewOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        // state.approvalURL = action.payload.approvalURL;
+        state.orderId = action.payload.orderId;
+        sessionStorage.setItem(
+          "currentOrderId",
+          JSON.stringify(action.payload.orderId)
+        );
+      })
+      .addCase(createNewOrder.rejected, (state) => {
+        state.isLoading = false;
+        // state.approvalURL = null;
+        state.orderId = null;
+      })
+      .addCase(getAllOrdersByUserId.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersByUserId.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersByUserId.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(getOrderDetails.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(getOrderDetails.rejected, (state) => {
+        state.isLoading = false;
+        state.orderDetails = null;
+      })
+      .addCase(getAllOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersForAdmin.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export const { resetOrderDetails } = adminOrderSlice.actions;
+
+export default orderSlice.reducer;
